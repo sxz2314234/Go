5b8dc328ba87c086fb9af13466654a4b702b8e8f
sxz2314234 <3218635958@qq.com>
2024-01-21 19:33:00 +0800

    update

--- sushu/src/main.go
+++ sushu/src/main.go
@@ -2,23 +2,24 @@ package main
 
 import "fmt"
 
-var a string = "Hello World"
-var b bool = true
+func sushu(obj int) {
+	var i, j int
+
+	for i = 2; i < obj; i++ {
+		for j = 2; j < i; j++ {
+			if i%j == 0 {
+				break
+			}
+		}
+
+		if j == i {
+			fmt.Printf("%d 是素数\n", i)
+		}
+	}
+}
 
 func main() {
-	var x int = 3
-	switch i := x.(type) {
-	case nil:
-		fmt.Printf("x 的类型是 :%T", i)
-	case int:
-		fmt.Printf("x 是 int 型")
-	case float64:
-		fmt.Printf("x 是 float64 型")
-	case func(int) float64:
-		fmt.Printf("x 是 func(int)")
-	case bool, string:
-		fmt.Printf("x 是 bool或 string 型")
-	default:
-		fmt.Printf("未知型")
-	}
+	var count int = 100
+
+	sushu(count)
 }
